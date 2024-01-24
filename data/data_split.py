0210fee4aa03aaadf39cd806474d4bea42ec7421
Mingyu <myang250@connect.hkust-gz.edu.cn>
2024-01-24 09:18:47 +0800

    modify new data split

--- data/data_split.py
+++ data/data_split.py
@@ -5,7 +5,7 @@ import numpy as np
 import faiss
 
 source = '/home/DATA/vector_data/'
-datasets = ['sift10m']
+datasets = ['sift10m','tiny5m','glove2.2m','word2vec']
 
 
 def do_compute_gt(xb, xq, topk=100):
@@ -22,18 +22,19 @@ if __name__ == "__main__":
         print(f'current dataset: {dataset}')
         path = os.path.join(source, dataset)
         base_path = os.path.join(path, f'{dataset}_base.fvecs')
-        query_path = os.path.join(path, f'{dataset}_query.fvecs')
         origin_data = fvecs_read(base_path)
         np.random.shuffle(origin_data)
-        query_data = fvecs_read(query_path)
         origin_num = origin_data.shape[0]
-        learn_num = int(1e5)
-        base_data = origin_data[:-learn_num]
-        learn_data = origin_data[-learn_num:]
+        learn_num = int(1e5) - int(1e3)
+        query_num = int(1e3)
+        split_num = int(1e5)
+        base_data = origin_data[:-split_num]
+        learn_data = origin_data[-split_num:query_num]
+        query_data = origin_data[-query_num:]
 
         gt = do_compute_gt(base_data, query_data, topk=100)
+        learn_gt = do_compute_gt(base_data, learn_data, topk=100)
         save_path = os.path.join(source, f'_{dataset}')
-
         if not os.path.exists(save_path):
             os.makedirs(save_path)
 
@@ -41,8 +42,10 @@ if __name__ == "__main__":
         save_learn_path = os.path.join(save_path, f'_{dataset}_learn.fvecs')
         save_query_path = os.path.join(save_path, f'_{dataset}_query.fvecs')
         save_ground_path = os.path.join(save_path, f'_{dataset}_groundtruth.ivecs')
+        save_learn_ground_path = os.path.join(save_path, f'_{dataset}_learn_groundtruth.ivecs')
 
         fvecs_write(save_base_path, base_data)
         fvecs_write(save_learn_path, learn_data)
         fvecs_write(save_query_path, query_data)
         ivecs_write(save_ground_path, gt)
+        ivecs_write(save_learn_ground_path, learn_gt)
--- run.sh
+++ run.sh
@@ -7,12 +7,12 @@ cd ./script
 #bash index_ivf.sh
 #
 #bash index_hnsw.sh
-#
-#bash index_pca.sh
-#
-#bash index_opq.sh
 
-#bash linear.sh
+bash index_pca.sh
+
+bash index_opq.sh
+
+bash linear.sh
 
 bash search_ivf.sh
 
